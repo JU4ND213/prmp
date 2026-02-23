81e7d9a95d6e8cabcbd722a6b4fc45cee6789b04
Ju4n <152440739+JU4ND213@users.noreply.github.com>
2026-02-23 16:20:44 -0500

    ARREGLOS

--- src/views/MapView.jsx
+++ src/views/MapView.jsx
@@ -37,8 +37,6 @@ export default function MapView() {
       dibujarPuntos,
       toggleMarcadoresBase,
       dibujarRutaDesdeGps,
-      rotarMapa,
-      resetearNorte,
       actualizarIdiomaBase,
       cleanup
     } = mapApi;
