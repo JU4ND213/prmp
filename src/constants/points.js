d65f0a2514a5f977c65fb190ea1324e63aa9936f
Ju4n <152440739+JU4ND213@users.noreply.github.com>
2026-02-24 15:10:40 -0500

    correcion visual points

--- src/constants/points.js
+++ src/constants/points.js
@@ -1,55 +1,55 @@
 // src/constants/points.js
 
 export const POINTS_BY_COLOR = {
-  // 🟥 Servicios 
+  // Servicios 
   servicios: [
-    { name: "Centro Médico", lat: -0.0024711, lng: -78.4541513, color: "#ff5252" },
+    { name: "Centro Médico", lat: -0.0024711, lng: -78.4541513, color: "#fe0000" },
     { name: "Lactario", lat: -0.0024388, lng: -78.4541916, color: "#ffd600" },
-    { name: "Cajero banco Guayaquil", lat: -0.0024103, lng: -78.4545376, color: "#01579b" },
+    { name: "Cajero banco Guayaquil", lat: -0.0024103, lng: -78.4545376, color: "#c90b77" },
   ],
 
-  // 🟨 Baños
+  // Baños
   banos: [
     { name: "Baño", description: "Lugar para hacer necesidades biológicas", lat: -0.002868, lng: -78.4540173, color: "#4d4706" },
-    { name: "Baño", lat: -0.0025649, lng: -78.4555165, color: "#ffea00" },
-    { name: "Baño", lat: -0.0018595, lng: -78.45485, color: "#ffea00" },
-    { name: "Baño", lat: -0.0015322, lng: -78.4557687, color: "#ffea00" }, 
+    { name: "Baño", lat: -0.0025649, lng: -78.4555165, color: "#00e1ff" },
+    { name: "Baño", lat: -0.0018595, lng: -78.45485, color: "#00e1ff" },
+    { name: "Baño", lat: -0.0015322, lng: -78.4557687, color: "#00e1ff" }, 
   ],
 
-  // 🟦 Atracciones y Museos
+  // Atracciones y Museos
   atracciones_museos: [
-    { name: "Boletería Mitad del Mundo", lat: -0.0032344, lng: -78.4538643, color: "#0288d1" },
-    { name: "Pabellón Guayasamin", lat: -0.0030775, lng: -78.454844, color: "#0288d1" },
-    { name: "Planetarium", lat: -0.002893, lng: -78.4550475, color: "#0288d1" },
-    { name: "Museo del cacao", lat: -0.001614, lng: -78.4551451, color: "#0288d1" },
-    { name: "Legado Virtual", lat: -0.0026843, lng: -78.4552738, color: "#0288d1" },
-    { name: "Experimento del Huevo", lat: -0.0021183, lng: -78.4550887, color: "#0288d1" },
-    { name: "Museo Cerveza Artesanal", lat: -0.0017324, lng: -78.4545912, color: "#0288d1" },
-    { name: "Pabellón de Francia", lat: -0.0031023, lng: -78.4550493, color: "#0288d1" },
-    { name: "Pabellón Ecuador", lat: -0.0029697, lng: -78.4552645, color: "#0288d1" },
-    { name: "Iglesia Mitad del Mundo", lat: -0.0021777, lng: -78.4543686, color: "#0288d1" },
-    { name: "Tiangues", lat: -0.002409, lng: -78.4545765, color: "#0288d1" },
-    { name: "Zona de llamas", lat: -0.0014521, lng: -78.4567139, color: "#0288d1" },
-    { name: "Tren Mitad del Mundo", lat: -0.0016097, lng: -78.4541643, color: "#0288d1" },
-    { name: "Viviendas ancestrales", lat: -0.0026562, lng: -78.4537317, color: "#0288d1" },
-    { name: "Viviendas Ancestrales - Ciudad Mitad del Mundo", lat: -0.0026649, lng: -78.4536884, color: "#0288d1" },
-    { name: "Inti Raymi Fiesta del sol", lat: -0.0029441, lng: -78.454315, color: "#0288d1" },
+    { name: "Boletería Mitad del Mundo", lat: -0.0032344, lng: -78.4538643, color: "#ffc800" },
+    { name: "Pabellón Guayasamin", lat: -0.0030775, lng: -78.454844, color: "#ffc800" },
+    { name: "Planetario", lat: -0.002893, lng: -78.4550475, color: "#ffc800" },
+    { name: "Museo del cacao", lat: -0.001614, lng: -78.4551451, color: "#ffc800" },
+    { name: "Legado Virtual", lat: -0.0026843, lng: -78.4552738, color: "#ffc800" },
+    { name: "Experimento del Huevo", lat: -0.0021183, lng: -78.4550887, color: "#ffc800" },
+    { name: "Museo Cerveza Artesanal", lat: -0.0017324, lng: -78.4545912, color: "#ffc800" },
+    { name: "Pabellón de Francia", lat: -0.0031023, lng: -78.4550493, color: "#ffc800" },
+    { name: "Pabellón Ecuador", lat: -0.0029697, lng: -78.4552645, color: "#ffc800" },
+    { name: "Iglesia Mitad del Mundo", lat: -0.0021777, lng: -78.4543686, color: "#ffc800" },
+    { name: "Tiangues", lat: -0.002409, lng: -78.4545765, color: "#ffc800" },
+    { name: "Zona de llamas", lat: -0.0014521, lng: -78.4567139, color: "#ffc800" },
+    { name: "Tren Mitad del Mundo", lat: -0.0016097, lng: -78.4541643, color: "#ffc800" },
+    { name: "Viviendas ancestrales", lat: -0.0026562, lng: -78.4537317, color: "#ffc800" },
+    { name: "Viviendas Ancestrales - Ciudad Mitad del Mundo", lat: -0.0026649, lng: -78.4536884, color: "#ffc800" },
+    { name: "Inti Raymi Fiesta del sol", lat: -0.0029441, lng: -78.454315, color: "#ffc800" },
   ],
 
-  // 🟧 Restaurantes
+  // Restaurantes
   restaurantes: [
-    { name: "Ñuca Llacta Restaurant", lat: -0.0024445, lng: -78.4543661, color: "#f9a825" },
-    { name: "RESTAURANTE", lat: -0.0020385, lng: -78.4544344, color: "#f9a825" },
-    { name: "Calima", lat: -0.0015815, lng: -78.4549402, color: "#f9a825" },
-    { name: "Café Sinchi Warmi", lat: -0.0017107, lng: -78.4552719, color: "#f9a825" },
-    { name: "raqeri cacao", lat: -0.0015384, lng: -78.4553481, color: "#f9a825" },
-    { name: "Restaurante Balcon al Mundo", lat: -0.0024231, lng: -78.4547545, color: "#f9a825" },
-    { name: "El Orgánico Star Coffee", lat: -0.0018819, lng: -78.4545454, color: "#f9a825" },
-    { name: "Café Guayasamin", lat: -0.0022184, lng: -78.4550409, color: "#f9a825" },
-    { name: "Tienda Delicatessen Rosita", lat: -0.0022465, lng: -78.4540331, color: "#f9a825" },
+    { name: "Ñuca Llacta Restaurant", lat: -0.0024445, lng: -78.4543661, color: "#583700" },
+    { name: "RESTAURANTE", lat: -0.0020385, lng: -78.4544344, color: "#583700" },
+    { name: "Calima", lat: -0.0015815, lng: -78.4549402, color: "#583700" },
+    { name: "Café Sinchi Warmi", lat: -0.0017107, lng: -78.4552719, color: "#583700" },
+    { name: "raqeri cacao", lat: -0.0015384, lng: -78.4553481, color: "#583700" },
+    { name: "Restaurante Balcon al Mundo", lat: -0.0024231, lng: -78.4547545, color: "#583700" },
+    { name: "El Orgánico Star Coffee", lat: -0.0018819, lng: -78.4545454, color: "#583700" },
+    { name: "Café Guayasamin", lat: -0.0022184, lng: -78.4550409, color: "#583700" },
+    { name: "Tienda Delicatessen Rosita", lat: -0.0022465, lng: -78.4540331, color: "#583700" },
   ],
 
-  // 🟪 Tiendas
+  // Tiendas
   tiendas_artesanias: [
     { name: "Piel y Cuero", lat: -0.0019065, lng: -78.4543313, color: "#9c27b0" },
     { name: "Local 27 - Native Crafts", lat: -0.0019129, lng: -78.4546417, color: "#9c27b0" },
@@ -64,28 +64,28 @@ export const POINTS_BY_COLOR = {
     { name: "Tienda Pichincha", lat: -0.0019104, lng: -78.4550271, color: "#9c27b0" },
   ],
 
-  // 🟩 Plazas
+  // Plazas
   plazas: [
-    { name: "Plaza Cultura", lat: -0.0021539, lng: -78.4537988, color: "#afb42b" },
-    { name: "Plaza Central", lat: -0.0021901, lng: -78.4546825, color: "#afb42b" },
-    { name: "Plaza de colores", lat: -0.00217, lng: -78.4541609, color: "#afb42b" },
-    { name: "Arena top", lat: -0.0013358, lng: -78.4569879, color: "#afb42b" },
+    { name: "Plaza Cultura", lat: -0.0021539, lng: -78.4537988, color: "#2bb446" },
+    { name: "Plaza Central", lat: -0.0021901, lng: -78.4546825, color: "#2bb446" },
+    { name: "Plaza de colores", lat: -0.00217, lng: -78.4541609, color: "#2bb446" },
+    { name: "Arena top", lat: -0.0013358, lng: -78.4569879, color: "#2bb446" },
   ],
 
-  // 🟦 Estacionamiento
+  // Estacionamiento
   estacionamiento: [
-    { name: "Ciudad Mitad del mundo - Estacionamiento", lat: -0.0026365, lng: -78.45301, color: "#0288d1" },
-    { name: "Parking", lat: -0.0014961, lng: -78.4536585, color: "#0288d1" },
+    { name: "Ciudad Mitad del mundo - Estacionamiento", lat: -0.0026365, lng: -78.45301, color: "#0209d1" },
+    { name: "Parking", lat: -0.0014961, lng: -78.4536585, color: "#0209d1" },
   ],
 };
 
 // MEJORA: Objeto de configuración para Títulos y Colores de botones
 export const CATEGORY_DETAILS = {
-  servicios: { label: "Servicios", color: "#ff5252" },
-  banos: { label: "Baños", color: "#ffea00" },
-  atracciones_museos: { label: "Atracciones", color: "#0288d1" },
-  restaurantes: { label: "Comida", color: "#f9a825" },
+  servicios: { label: "Servicios", color: "#ff0000" },
+  banos: { label: "Baños", color: "#00e1ff" },
+  atracciones_museos: { label: "Atracciones", color: "#ffc800" },
+  restaurantes: { label: "Comida", color: "#583700" },
   tiendas_artesanias: { label: "Artesanías", color: "#9c27b0" },
-  plazas: { label: "Plazas", color: "#afb42b" },
-  estacionamiento: { label: "Parking", color: "#607d8b" }, // Cambié color para distinguir
+  plazas: { label: "Plazas", color: "#2bb446" },
+  estacionamiento: { label: "Parking", color: "#0209d1" }, // Cambié color para distinguir
 };
